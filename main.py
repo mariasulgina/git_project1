6ea63cebacc19081b7c229288c8ca0f3ced4bc65
mariasulgina <151562649+mariasulgina@users.noreply.github.com>
2023-11-20 21:31:52 +0300

    Update main.py
    update

--- main.py
+++ main.py
@@ -2,13 +2,32 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
-from PyQt5.uic import loadUi
+import random
 
-class MyWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QGraphicsView(MainWindow)
+        self.centralwidget.setGeometry(10, 10, 381, 192)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(160, 210, 75, 23)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить окружность"))
+
+class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('UI.ui', self)
-
+        self.setupUi(self)
         self.scene = QGraphicsScene()
         self.view = QGraphicsView(self.scene)
         self.setCentralWidget(self.view)
@@ -16,10 +35,11 @@ class MyWindow(QMainWindow):
         self.pushButton.clicked.connect(self.add_circle)
 
     def add_circle(self):
-        diameter = 50
+        diameter = random.randint(20, 100)
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         ellipse = QGraphicsEllipseItem(0, 0, diameter, diameter)
         ellipse.setPos(10, 10)
-        ellipse.setBrush(QColor(255, 255, 0))  # желтый цвет
+        ellipse.setBrush(color)
         self.scene.addItem(ellipse)
 
 if __name__ == '__main__':
@@ -27,4 +47,3 @@ if __name__ == '__main__':
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
-
